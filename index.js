989f2b1ff8aaa69c5cf9c122aabc5f2e75814301
Christoph <christoph@aufmhoff.com>
2020-04-03 01:01:52 +0200

    Der Wendler v0.1

---
+++ index.js
@@ -0,0 +1,88 @@
+var SlackBot = require('slackbots');
+const dotenv = require('dotenv')
+
+dotenv.config()
+// create a bot
+var bot = new SlackBot({
+    token: `${process.env.BOT_TOKEN}`, // Add a bot https://my.slack.com/services/new/bot and put the token 
+    name: 'Der Wendler'
+});
+ 
+bot.on('start', function() {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':cat:'
+    };
+    
+    // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
+   // bot.postMessageToChannel('general', 'meow!', params);
+    
+    // define existing username instead of 'user_name'
+    bot.postMessageToUser('user_name', 'meow!', params); 
+    
+    // If you add a 'slackbot' property, 
+    // you will post to another user's slackbot channel instead of a direct message
+    bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
+    
+    // define private group instead of 'private_group', where bot exist
+    bot.postMessageToGroup('private_group', 'meow!', params); 
+});
+
+bot.on("message", msg => {
+  switch (msg.type) {
+  case "message":
+    if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+		
+		
+		if(msg.text.includes("Sentry") )
+		{
+
+			  bot.getUsers().then(function(data){
+				   
+				   console.dir(data);
+				   var rnd = Math.floor(Math.random()*data.members.length);
+				   console.log(rnd);
+				   var user = data.members[rnd].name;
+				   console.log(user);
+				   bot.postMessage(msg.user, user , { as_user: true })
+				  
+			  });
+			 
+		}
+		
+	    if(msg.text.includes("Egal") )
+		{
+
+			  bot.getUsers().then(function(data){
+				   
+
+				   postToUserWithImage(msg,"https://media1.giphy.com/media/ZG5KTqutRAfZ6i5OVR/giphy.gif?cid=ecf05e47d1386acc4c79affd330d8fa4705ef201326fe40a&rid=giphy.gif");
+				  
+			  });
+			 
+		}
+		
+    
+    }
+    break
+  }
+});
+
+
+function postToUserWithImage(message, picUrl) {
+    bot.postMessage(
+        message.user,
+        'Egal!',
+        params = {
+			"as_user":true,
+            "icon_url": picUrl,
+            "attachments":
+                [
+                    {
+                        "fallback": "this did not work",
+                        "image_url": picUrl
+                    }
+                ]
+        }
+    )
+}
